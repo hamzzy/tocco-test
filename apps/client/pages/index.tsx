dacbf4ceab8f556ec08d5e4cc9d894fd72e05758
ibrahim Hamzat <hamat.ibrahim3@gmail.com>
2024-05-21 11:41:44 +0100

    update docker composer, add crud for product

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
-  </p>
+          </p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
